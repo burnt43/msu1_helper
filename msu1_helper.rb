837ee48d8573f948a2af8abae42d0eccbd1512e6
burnt43 <jms.crsn@gmail.com>
2018-10-23 17:31:13 -0400

    introduce loop table class

--- msu1_helper.rb
+++ msu1_helper.rb
@@ -8,15 +8,15 @@ class ArgumentParser
 
   def self.parse(args)
     options = OpenStruct.new
-    options.input               = nil
-    options.output_type         = nil
-    options.loop_start          = nil
-    options.loop_end            = nil
-    options.loop_table_filename = nil
+    options.input       = nil
+    options.output_type = nil
+    options.loop_start  = nil
+    options.loop_end    = nil
+    options.loop_table  = nil
 
     option_parser = OptionParser.new {|opts|
       opts.on("-i", "--input=INPUT", "input file or glob") {|input|
-        options.input = input
+        options.input = Dir[input]
       }
 
       opts.on("-t", "--output-type=OUTPUT_TYPE", "type to convert audio to (#{OUTPUT_TYPES.join(', ')})") {|output_type|
@@ -31,8 +31,8 @@ class ArgumentParser
         options.loop_end = loop_end
       }
 
-      opts.on("-l", "--loop-table=LOOP_TABLE", "loop table filename") {|loop_table_filename|
-        options.loop_table_filename = loop_table_filename
+      opts.on("-l", "--loop-table=LOOP_TABLE_FILENAME", "loop table filename") {|loop_table_filename|
+        options.loop_table = File.exists?(loop_table_filename) ? LoopTable.new(loop_table_filename) : nil
       }
 
       opts.on_tail("-h", "--help", "show this message") do
@@ -58,19 +58,30 @@ class ArgumentParser
       print_error("no output_type was given. please specify using -t or --output-type")
     end
 
-    files_to_operate_on = Dir[options.input]
-
-    if files_to_operate_on.empty?
+    # no input files
+    if options.input.empty?
       print_error("could not find any input files.")
+    # 1 input file
+    elsif options.input.length == 1
+      if (options.loop_start.nil? || options.loop_end.nil?)
+        print_error("you want to convert a single file to msu1pcm, but have not given a LOOP START or LOOP END. please specify using -s or --loop-start and -e or --loop-end.")
+      end
+    # multiple input files
+    else
+      if options.output_type == 'msu1pcm'
+        if options.loop_table.nil?
+          print_error("you want you convert many files to msu1pcm, but you have not provided a loop_table file. please specift using -l or --loop-table")
+        else
+          options.input
+            .reject{|filename| options.loop_table.has_filename?(filename)}
+            .each{|filename| print_warning("#{filename} is not in the loop table. it will be ignored and not be converted.")}
+        end
+      end
     end
+  end
 
-    if files_to_operate_on.length == 1 && (options.loop_start.nil? || options.loop_end.nil?)
-      print_error("you want to convert a single file to msu1pcm, but have not given a LOOP START or LOOP END. please specify using -s or --loop-start and -e or --loop-end.")
-    end
-
-    if files_to_operate_on.length > 1 && options.output_type == 'msu1pcm' && options.loop_table_filename.nil?
-      print_error("you want you convert many files to msu1pcm, but you have not provided a loop_table file.")
-    end
+  def self.print_warning(warning_message)
+    puts "[\033[0;33mWARNING\033[0;0m] - #{warning_message}"
   end
 
   def self.print_error(error_message)
@@ -79,18 +90,37 @@ class ArgumentParser
   end
 end # ArgumentParser
 
-class Msu1Helper
+class LoopTable
+  def initialize(filename)
+    @data = Hash.new
+
+    File.open(filename, 'r') {|f|
+      f.each_line {|line|
+        filename, loop_start, loop_end = line.split(',')
+        @data[filename] = {
+          loop_start: loop_start,
+          loop_end:   loop_end,
+        }
+      }
+    }
+  end
+
+  def has_filename?(filename)
+    @data.has_key?(Pathname.new(filename).basename.to_s)
+  end
+end # LoopTable
+
+class FileConvertor
   FFMPEG_BIN = '/usr/bin/ffmpeg'
 
   def initialize(input_filename, options={})
     @input_filename    = input_filename
 
-    @msu1_pcm_filename        = options[:msu1_pcm_filename]
     @loop_start_sample_number = options[:loop_start_sample_number]
     @loop_end_sample_number   = options[:loop_end_sample_number]
   end
   
-  def convert_wav_to_msu1_pcm!
+  def convert_wav_to_msu1pcm!
     trimmed_filename = trim_wav_file!(@input_filename)
     raw_filename     = rawify_wav_file!(trimmed_filename)
     
@@ -98,7 +128,7 @@ class Msu1Helper
     raw_file_contents = raw_file.read
     raw_file.close
 
-    File.open(@msu1_pcm_filename, 'w') {|f|
+    File.open(msu1pcm_filename, 'w') {|f|
       f.print('MSU1')
       f.print(loop_start_sample_number_little_endian_bytes.map(&:chr).join)
       f.print(raw_file_contents)
@@ -107,6 +137,13 @@ class Msu1Helper
 
   def convert_to_wav!
     wavify_file!(@input_filename)
+  end
+
+  def msu1pcm_filename
+    pathname        = Pathname.new(@input_filename)
+    output_filename = "#{pathname.dirname}/#{pathname.basename('.*')}.pcm"
+
+    output_filename
   end
 
   private
@@ -149,29 +186,23 @@ class Msu1Helper
       bit_string[starting_position..ending_position].to_i(2)
     }
   end
-end # Msu1Helper
+end # FileConvertor
 
-ArgumentParser.parse(ARGV)
+class Msu1Helper
+  def initialize(options)
+    @options = options
+  end
+
+  def execute!
+  end
+end
+
+options = ArgumentParser.parse(ARGV)
+#puts Msu1Helper.new('/home/jcarson/msu1_audio/snes/ff6/103_Awakening.wav').msu1pcm_filename
+
+
 =begin
-Msu1Helper.new('/home/jcarson/msu1_audio/nes/10_track_10.mp3').convert_to_wav!
-Msu1Helper.new('/home/jcarson/msu1_audio/nes/11_track_11.mp3').convert_to_wav!
-Msu1Helper.new('/home/jcarson/msu1_audio/nes/12_track_12.mp3').convert_to_wav!
-Msu1Helper.new('/home/jcarson/msu1_audio/nes/13_track_13.mp3').convert_to_wav!
-Msu1Helper.new('/home/jcarson/msu1_audio/nes/14_track_14.mp3').convert_to_wav!
-Msu1Helper.new('/home/jcarson/msu1_audio/nes/15_track_15.mp3').convert_to_wav!
-Msu1Helper.new('/home/jcarson/msu1_audio/nes/16_track_16.mp3').convert_to_wav!
-Msu1Helper.new('/home/jcarson/msu1_audio/nes/1_track_1.mp3').convert_to_wav!
-Msu1Helper.new('/home/jcarson/msu1_audio/nes/2_track_2.mp3').convert_to_wav!
-Msu1Helper.new('/home/jcarson/msu1_audio/nes/3_track_3.mp3').convert_to_wav!
-Msu1Helper.new('/home/jcarson/msu1_audio/nes/4_track_4.mp3').convert_to_wav!
-Msu1Helper.new('/home/jcarson/msu1_audio/nes/5_track_5.mp3').convert_to_wav!
-Msu1Helper.new('/home/jcarson/msu1_audio/nes/6_track_6.mp3').convert_to_wav!
-Msu1Helper.new('/home/jcarson/msu1_audio/nes/7_track_7.mp3').convert_to_wav!
-Msu1Helper.new('/home/jcarson/msu1_audio/nes/8_track_8.mp3').convert_to_wav!
-Msu1Helper.new('/home/jcarson/msu1_audio/nes/9_track_9.mp3').convert_to_wav!
-
 Msu1Helper.new('/home/jcarson/msu1_audio/snes/ff6/103_Awakening.wav',{
-  msu1_pcm_filename:         '/home/jcarson/msu1_audio/snes/ff6/alttp_msu.pcm',
   loop_start_sample_number:  846_099,
   loop_end_sample_number:    2_540_145,
 }).convert_wav_to_msu1_pcm!
