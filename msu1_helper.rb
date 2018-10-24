f9f696ced521b9120e1a624e0654e8ecc40b29c8
burnt43 <jms.crsn@gmail.com>
2018-10-23 20:30:27 -0400

    change a bunch of stuff to get conversion en masse working.

--- msu1_helper.rb
+++ msu1_helper.rb
@@ -2,21 +2,23 @@
 require 'pathname'
 require 'optparse'
 require 'ostruct'
+require 'fileutils'
 
 class ArgumentParser
-  OUTPUT_TYPES = %w(wav_pcm_s16le msu1pcm)
+  OUTPUT_TYPES = %w(wav_pcm_s16le msu1_pcm)
 
   def self.parse(args)
     options = OpenStruct.new
-    options.input       = nil
+    options.input_files = nil
     options.output_type = nil
     options.loop_start  = nil
     options.loop_end    = nil
     options.loop_table  = nil
+    options.destdir     = nil
 
     option_parser = OptionParser.new {|opts|
       opts.on("-i", "--input=INPUT", "input file or glob") {|input|
-        options.input = Dir[input]
+        options.input_files = Dir[input]
       }
 
       opts.on("-t", "--output-type=OUTPUT_TYPE", "type to convert audio to (#{OUTPUT_TYPES.join(', ')})") {|output_type|
@@ -35,6 +37,10 @@ class ArgumentParser
         options.loop_table = File.exists?(loop_table_filename) ? LoopTable.new(loop_table_filename) : nil
       }
 
+      opts.on("-d", "--destdir=DESTINATION_DIRECTORY", "write converted files to this directory") {|destdir|
+        options.destdir = destdir
+      }
+
       opts.on_tail("-h", "--help", "show this message") do
         puts opts
         exit
@@ -50,7 +56,7 @@ class ArgumentParser
   private
 
   def self.validate_options(options)
-    unless options.input
+    unless options.input_files
       print_error("no input was given. please specify using -i or --input.")
     end
 
@@ -59,20 +65,20 @@ class ArgumentParser
     end
 
     # no input files
-    if options.input.empty?
+    if options.input_files.empty?
       print_error("could not find any input files.")
     # 1 input file
-    elsif options.input.length == 1
+    elsif options.input_files.length == 1
       if (options.loop_start.nil? || options.loop_end.nil?)
-        print_error("you want to convert a single file to msu1pcm, but have not given a LOOP START or LOOP END. please specify using -s or --loop-start and -e or --loop-end.")
+        print_error("you want to convert a single file to msu1_pcm, but have not given a LOOP START or LOOP END. please specify using -s or --loop-start and -e or --loop-end.")
       end
     # multiple input files
     else
-      if options.output_type == 'msu1pcm'
+      if options.output_type == 'msu1_pcm'
         if options.loop_table.nil?
-          print_error("you want you convert many files to msu1pcm, but you have not provided a loop_table file. please specift using -l or --loop-table")
+          print_error("you want you convert many files to msu1_pcm, but you have not provided a loop_table file. please specift using -l or --loop-table")
         else
-          options.input
+          options.input_files
             .reject{|filename| options.loop_table.has_filename?(filename)}
             .each{|filename| print_warning("#{filename} is not in the loop table. it will be ignored and not be converted.")}
         end
@@ -96,7 +102,11 @@ class LoopTable
 
     File.open(filename, 'r') {|f|
       f.each_line {|line|
-        filename, loop_start, loop_end = line.split(',')
+        values     = line.strip.split(',')
+        filename   = values[0]
+        loop_start = values[1].to_i
+        loop_end   = values[2].to_i
+
         @data[filename] = {
           loop_start: loop_start,
           loop_end:   loop_end,
@@ -106,7 +116,17 @@ class LoopTable
   end
 
   def has_filename?(filename)
-    @data.has_key?(Pathname.new(filename).basename.to_s)
+    @data.has_key?(key_from_filename(filename))
+  end
+
+  def loop_bounds_by_filename(filename)
+    @data[key_from_filename(filename)]
+  end
+
+  private
+
+  def key_from_filename(filename)
+    Pathname.new(filename).basename.to_s
   end
 end # LoopTable
 
@@ -118,9 +138,12 @@ class FileConvertor
 
     @loop_start_sample_number = options[:loop_start_sample_number]
     @loop_end_sample_number   = options[:loop_end_sample_number]
+    @destdir                  = options[:destdir]
   end
   
-  def convert_wav_to_msu1pcm!
+  def convert_wav_to_msu1_pcm!
+    create_destdir
+
     trimmed_filename = trim_wav_file!(@input_filename)
     raw_filename     = rawify_wav_file!(trimmed_filename)
     
@@ -128,47 +151,47 @@ class FileConvertor
     raw_file_contents = raw_file.read
     raw_file.close
 
-    File.open(msu1pcm_filename, 'w') {|f|
+    File.open(msu1_pcm_filename, 'w') {|f|
       f.print('MSU1')
       f.print(loop_start_sample_number_little_endian_bytes.map(&:chr).join)
       f.print(raw_file_contents)
     }
+
+    FileUtils.rm(trimmed_filename)
+    FileUtils.rm(raw_filename)
   end
 
   def convert_to_wav!
+    create_destdir
+
     wavify_file!(@input_filename)
   end
 
-  def msu1pcm_filename
-    pathname        = Pathname.new(@input_filename)
-    output_filename = "#{pathname.dirname}/#{pathname.basename('.*')}.pcm"
+  private
 
+  def msu1_pcm_filename
+    output_filename = "#{destdir}/#{output_file_basename}.pcm"
     output_filename
   end
-
-  private
   
   def wavify_file!(input_filename)
-    pathname        = Pathname.new(@input_filename)
-    output_filename = "#{pathname.dirname}/#{pathname.basename('.*')}.wav"
+    output_filename = "#{destdir}/#{output_file_basename}.wav"
 
-    %x[#{FFMPEG_BIN} -y -i #{input_filename} -acodec pcm_s16le #{output_filename}]
+    execute_system_command("#{FFMPEG_BIN} -y -i #{input_filename} -acodec pcm_s16le #{output_filename}")
     output_filename
   end
 
   def trim_wav_file!(input_filename)
-    pathname        = Pathname.new(@input_filename)
-    output_filename = "#{pathname.dirname}/#{pathname.basename('.*')}_trim#{pathname.extname}"
+    output_filename = "#{destdir}/#{output_file_basename}_trim#{input_file_ext}"
 
-    %x[#{FFMPEG_BIN} -y -i #{input_filename} -af atrim=start_sample=0:end_sample=#{@loop_end_sample_number} #{output_filename}]
+    execute_system_command("#{FFMPEG_BIN} -y -i #{input_filename} -af atrim=start_sample=0:end_sample=#{@loop_end_sample_number} #{output_filename}")
     output_filename
   end
 
   def rawify_wav_file!(input_filename)
-    pathname        = Pathname.new(@input_filename)
-    output_filename = "#{pathname.dirname}/#{pathname.basename('.*')}.raw"
+    output_filename = "#{destdir}/#{output_file_basename}.raw"
 
-    %x[#{FFMPEG_BIN} -y -i #{input_filename} -f s16le -c:a pcm_s16le #{output_filename}]
+    execute_system_command("#{FFMPEG_BIN} -y -i #{input_filename} -f s16le -c:a pcm_s16le #{output_filename}")
     output_filename
   end
 
@@ -186,6 +209,27 @@ class FileConvertor
       bit_string[starting_position..ending_position].to_i(2)
     }
   end
+
+  def execute_system_command(string)
+    puts "\033[0;32m#{string}\033[0;0m"
+    %x[#{string}]
+  end
+
+  def destdir
+    @destdir || Pathname.new(@input_filename).dirname
+  end
+
+  def create_destdir
+    FileUtils.mkdir_p(destdir)
+  end
+
+  def input_file_ext
+    Pathname.new(@input_filename).extname
+  end
+
+  def output_file_basename
+    Pathname.new(@input_filename).basename('.*')
+  end
 end # FileConvertor
 
 class Msu1Helper
@@ -194,16 +238,26 @@ class Msu1Helper
   end
 
   def execute!
+    case @options.output_type
+    when 'wav_pcm_s16le'
+      @options.input_files.each {|filename|
+        FileConvertor.new(filename,{
+          destdir: @options.destdir,
+        }).convert_to_wav!
+      }
+    when 'msu1_pcm'
+      @options.input_files.each {|filename|
+        if loop_bounds = @options.loop_table.loop_bounds_by_filename(filename)
+          FileConvertor.new(filename,{
+            loop_start_sample_number: loop_bounds[:loop_start],
+            loop_end_sample_number:   loop_bounds[:loop_end],
+            destdir:                  @options.destdir,
+          }).convert_wav_to_msu1_pcm!
+        end 
+      }
+    end
   end
-end
+end # Msu1Helper
 
 options = ArgumentParser.parse(ARGV)
-#puts Msu1Helper.new('/home/jcarson/msu1_audio/snes/ff6/103_Awakening.wav').msu1pcm_filename
-
-
-=begin
-Msu1Helper.new('/home/jcarson/msu1_audio/snes/ff6/103_Awakening.wav',{
-  loop_start_sample_number:  846_099,
-  loop_end_sample_number:    2_540_145,
-}).convert_wav_to_msu1_pcm!
-=end
+Msu1Helper.new(options).execute!
